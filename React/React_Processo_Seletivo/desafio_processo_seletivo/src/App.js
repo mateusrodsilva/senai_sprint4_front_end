2de1938fde266b3bbac17f84d442ddc67314b25b
Mateus Rodrigues Silva <73179530+Mateusrodrigues75@users.noreply.github.com>
2021-05-26 00:05:37 -0300

    erro no Render - Desafio Processo Seletivo

--- React/React_Processo_Seletivo/desafio_processo_seletivo/src/App.js
+++ React/React_Processo_Seletivo/desafio_processo_seletivo/src/App.js
@@ -1,24 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+import {render} from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Consumindo API do GitHub
-        </p>
-        <a
-          className="App-link"
-          href="https://localhost:3000/github_info"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Listando informações de um usuário do GitHub
-        </a>
-      </header>
+class InfoUserApp extends Component{
+  constructor(props){
+      super(props);
+      this.state = {
+        user : '',
+        repos: [],
+        name: ''
+      }
+  }
+}
+
+buscarUser = (event) => {
+  console.log('chamando API')
+
+  event.preventDefault();
+
+  fetch('https://api.github.com/users/' + this.state.user +'/repos?sort=created&per_page=1')
+
+  .then(resposta => resposta.json())
+
+  .then(data => this.setState({ repos : data }))
+
+  .catch( erro => console.log(erro) )
+}
+
+atualizaNomeUser = async (evento) => {
+await this.setState({ nome : evento.target.value })
+console.log(this.state.user)
+};
+
+
+render(){
+  return(
+    <div>
+      <main>
+        <section>
+          <h2>Info User</h2>
+          <form>
+            <input
+            type = 'text'
+            value= {this.state.user}
+            placeholder = "Insira um nome de usuário cadastrado no GitHub"
+            />
+            <button type = 'submit'>Pesquisar</button>
+          </form>
+          <section>
+            <h2>Dados de {this.state.name}</h2>
+            <table>
+              <tbody>
+                {
+                  this.state.repos.map((evento) => {
+                    return(
+                      <tr key = {evento.id}>
+                        <td>{evento.name}</td>
+                        <td>{evento.description}</td>
+                        <td>{evento.created_at}</td>
+                        <td>{evento.size}</td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </section>
+        </section>
+      </main>
     </div>
   );
 }
 
-export default App;
+export default InfoUserApp;
--- React/React_Processo_Seletivo/desafio_processo_seletivo/src/index.js
+++ React/React_Processo_Seletivo/desafio_processo_seletivo/src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom'
 
 import './index.css';
 
-import App from './App';
-import InfoUser from './info_user_github/info_user_github';
+import InfoUserApp from './App';
 import reportWebVitals from './reportWebVitals';
 
 const routing = (
   <Router>
     <div>
       <Switch>
-        <Route exact path="/" component={App} /> {/* Home */}
-        <Route path="/tiposeventos" component={InfoUser} /> {/* Info User */}
+        <Route exact path="/" component={InfoUserApp} /> {/* Home */}
       </Switch>
     </div>
   </Router>
